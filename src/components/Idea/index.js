3ab1960927ac8e31fb41757548111e8f2eafe42d
pearmini <subairui@icloud.com>
2020-08-03 16:42:06 +0800

    fix(Idea): drag image from Idea result in new or current tab

--- src/components/Idea/index.js
+++ src/components/Idea/index.js
@@ -6,19 +6,19 @@ import { MyImage } from "../Input";
 const { TextArea } = Input;
 export default connect(
   ({ slides }) => ({
-    selectedIdea: slides.selectedIdea
+    selectedIdea: slides.selectedIdea,
   }),
   {
-    deleteIdea: id => ({ type: "slides/deleteIdea", payload: { id } }),
+    deleteIdea: (id) => ({ type: "slides/deleteIdea", payload: { id } }),
     saveIdea: (id, value) => ({
       type: "slides/saveIdea",
-      payload: { id, value }
+      payload: { id, value },
     }),
-    setSelectedIdea: id => ({
+    setSelectedIdea: (id) => ({
       type: "slides/setSelectedIdea",
-      payload: { id }
+      payload: { id },
     }),
-    setHovered: id => ({ type: "global/setHovered", payload: { id } })
+    setHovered: (id) => ({ type: "global/setHovered", payload: { id } }),
   }
 )(function({
   content,
@@ -27,19 +27,19 @@ export default connect(
   selectedIdea,
   setSelectedIdea,
   setHovered,
-  id: elId
+  id: elId,
 }) {
   const { type, id, value } = content;
   const [edit, setEdit] = useState(false);
   const [enter, setEnter] = useState(false);
   const styles = {
     box: {
-      border: selectedIdea === id ? "1px solid #4091f7" : "1px solid #d9d9d9"
+      border: selectedIdea === id ? "1px solid #4091f7" : "1px solid #d9d9d9",
     },
     input: { height: "100%", width: "100%", resize: "none" },
     tool: {
-      opacity: enter && !edit ? 1 : 0
-    }
+      opacity: enter && !edit ? 1 : 0,
+    },
   };
 
   function handleDragStart(e, item) {
@@ -52,8 +52,8 @@ export default connect(
       id={elId}
       onClick={() => setSelectedIdea(id)}
       draggable
-      onDragStart={e => handleDragStart(e, content)}
-      onDragEnd={e => setHovered(-1)}
+      onDragStart={(e) => handleDragStart(e, content)}
+      onDragEnd={(e) => setHovered(-1)}
       className={classNames.container}
       onMouseLeave={() => setEnter(false)}
       onMouseEnter={() => setEnter(true)}
@@ -62,30 +62,38 @@ export default connect(
     >
       <div className={classNames.box} style={styles.box}>
         {type === "image" ? (
-          <img src={value} className={classNames.image} draggable />
+          <div
+            src={value}
+            className={classNames.image}
+            draggable
+            style={{
+              backgroundImage: `url(${value})`,
+              backgroundSize: "contain",
+              backgroundRepeat: "no-repeat",
+              height: "100%",
+              width: "100%",
+              backgroundPosition: "center center",
+            }}
+          />
         ) : edit ? (
           <TextArea
             type="textarea"
             style={styles.input}
             value={value}
-            onChange={e => saveIdea(id, e.target.value)}
+            onChange={(e) => saveIdea(id, e.target.value)}
           />
         ) : (
           <div className={classNames.text}>{value}</div>
         )}
         {edit && (
           <div className={classNames.save}>
-            <Icon
-              type="save"
-              onClick={() => setEdit(false)}
-              theme="filled"
-            ></Icon>
+            <Icon type="save" onClick={() => setEdit(false)} theme="filled" />
           </div>
         )}
         <div className={classNames.tool} style={styles.tool}>
           {type === "image" ? (
             <Popover
-              content={<MyImage onChange={value => saveIdea(id, value)} />}
+              content={<MyImage onChange={(value) => saveIdea(id, value)} />}
             >
               <Icon type="edit" theme="filled" />
             </Popover>
