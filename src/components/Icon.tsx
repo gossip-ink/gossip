e6f3bda800cbe03672ce17e3c3172f5241a27a75
tinypear <1347054683@qq.com>
2021-02-25 22:49:02 +0800

    feat(Icon): add selected props

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import { FontAwesomeIcon, FontAwesomeIconProps } from "@fortawesome/react-fontawesome";
 import classNames from "classnames";
 
-const Icon: React.FC<IconProps> = ({ className, ...restProps }) => {
-  const classes = classNames(className);
+const Icon: React.FC<IconProps> = ({ className, selected = true, ...restProps }) => {
+  const classes = classNames(className, {
+    "text-gray-400": !selected,
+    "hover:text-gray-500": !selected,
+  });
   return <FontAwesomeIcon className={classes} size="sm" {...restProps} />;
 };
 
 Icon.displayName = "Icon";
 
-export interface IconProps extends FontAwesomeIconProps {}
+export interface IconProps extends FontAwesomeIconProps {
+  selected?: boolean;
+}
 export default Icon;
