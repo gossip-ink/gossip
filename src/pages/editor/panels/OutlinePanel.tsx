a199063f9e674c205efe0f9fe9f383214df94438
tinypear <1347054683@qq.com>
2021-03-03 23:56:16 +0800

    feat(EditorPage): basic structure of OutlinePanel

--- src/pages/editor/panels/OutlinePanel.tsx
+++ src/pages/editor/panels/OutlinePanel.tsx
@@ -1,6 +1,15 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import Icon from "../../../components/Icon";
+import Tree, { nodes } from "../../../components/Tree";
+import { NodeType, PageNode } from "../../../models/node";
+import { OutlineNode } from "../../../models/file";
+import List from "../../../components/List";
+import { nanoid } from "nanoid";
+import OutlineBlock from "../components/OutlineBlock";
+import ThumbnailBlock from "../components/ThumbnailBlock";
+
+const { ListItem } = List;
 
 const Container = styled.div``;
 
@@ -9,6 +18,112 @@ const SwitchIcon = styled(Icon).attrs((props) => ({
 }))``;
 
 const OutlinePanel: React.FC<OutlinePanelProps> = (props) => {
+  const simpleTreeData: OutlineNode = {
+    id: nanoid(),
+    data: {
+      title: "0",
+      labels: [],
+      pageNodes: [
+        {
+          id: nanoid(),
+          type: NodeType.Page,
+          children: [
+            {
+              id: nanoid(),
+              type: NodeType.Text,
+              text: "A",
+            },
+          ],
+        },
+      ],
+    },
+    children: [
+      {
+        id: nanoid(),
+        data: {
+          title: "0-0",
+          labels: [],
+          pageNodes: [
+            {
+              id: nanoid(),
+              type: NodeType.Page,
+              children: [
+                {
+                  id: nanoid(),
+                  type: NodeType.Text,
+                  text: "B",
+                },
+              ],
+            },
+            {
+              id: nanoid(),
+              type: NodeType.Page,
+              children: [
+                {
+                  id: nanoid(),
+                  type: NodeType.Text,
+                  text: "C",
+                },
+              ],
+            },
+          ],
+        },
+        children: [
+          {
+            id: nanoid(),
+            data: {
+              labels: [],
+              title: "0-0-0",
+              pageNodes: [
+                {
+                  id: nanoid(),
+                  type: NodeType.Page,
+                  children: [
+                    {
+                      id: nanoid(),
+                      type: NodeType.Text,
+                      text: "D",
+                    },
+                  ],
+                },
+              ],
+            },
+            children: [],
+          },
+        ],
+      },
+      {
+        id: nanoid(),
+        data: {
+          title: "0-1",
+          labels: [],
+          pageNodes: [
+            {
+              id: nanoid(),
+              type: NodeType.Page,
+              children: [
+                {
+                  id: nanoid(),
+                  type: NodeType.Text,
+                  text: "E",
+                },
+              ],
+            },
+          ],
+        },
+        children: [],
+      },
+    ],
+  };
+
+  const simpleNodes: PageNode[] = nodes(simpleTreeData).reduce(
+    (total: PageNode[], cur: OutlineNode) => {
+      const nodes = cur.data.pageNodes ? cur.data.pageNodes : [];
+      return [...total, ...nodes];
+    },
+    []
+  );
+
   const [isOutlineMode, setIsOutlineMode] = useState<boolean>(true);
   const [isTreeThumbnail, setIsTreeThumbnail] = useState<boolean>(true);
 
@@ -40,6 +155,23 @@ const OutlinePanel: React.FC<OutlinePanelProps> = (props) => {
               onClick={() => setIsTreeThumbnail(false)}
             />
           </>
+        )}
+      </div>
+      <div>
+        {isOutlineMode || isTreeThumbnail ? (
+          <Tree
+            nested={!isOutlineMode && isTreeThumbnail}
+            data={simpleTreeData}
+            node={isOutlineMode ? <OutlineBlock /> : <ThumbnailBlock />}
+          />
+        ) : (
+          <List>
+            {simpleNodes.map((d) => (
+              <ListItem key={d.id}>
+                <ThumbnailBlock data={d} />
+              </ListItem>
+            ))}
+          </List>
         )}
       </div>
     </Container>
