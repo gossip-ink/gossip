bddf7913d540971bdab310a9599e8be1db48129a
pearmini <subairui@icloud.com>
2021-07-04 17:38:01 +0800

    feat(hooks): add useMeasure to get rect and clientRect of element

---
+++ src/hooks/useMeasure.ts
@@ -0,0 +1,47 @@
+import { useMeasure } from "react-use";
+import React, { useState, useEffect, useRef } from "react";
+
+type ClientRect = {
+  clientLeft: number;
+  clientRight: number;
+  clientTop: number;
+  clientBottom: number;
+  clientWidth: number;
+  clientHeight: number;
+};
+
+export default function () {
+  const [ref, rect] = useMeasure();
+  const elementRef = useRef<Element>();
+  const [clientRect, setClientRect] = useState<ClientRect>({
+    clientBottom: 0,
+    clientRight: 0,
+    clientLeft: 0,
+    clientTop: 0,
+    clientWidth: 0,
+    clientHeight: 0,
+  });
+
+  const newRef = (element: Element) => {
+    if (!element) return;
+    elementRef.current = element;
+    ref(element);
+  };
+
+  useEffect(() => {
+    const element = elementRef.current;
+    if (!element) return;
+    const rect = element.getBoundingClientRect();
+
+    setClientRect({
+      clientLeft: rect.left,
+      clientRight: rect.right,
+      clientBottom: rect.bottom,
+      clientTop: rect.top,
+      clientWidth: rect.width,
+      clientHeight: rect.height,
+    });
+  }, [rect]);
+
+  return { ref: newRef, rect: { ...rect, ...clientRect } };
+}
